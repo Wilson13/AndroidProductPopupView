a5d93a3361d133de6be37c7a531709dd2c56802b
Wilson Ow <wilsonxin@hotmail.com>
2016-10-06 14:09:57 +0800

    Changed card display UI and added "buy now" button

--- app/src/main/java/sg/com/bitwave/shoppingsystem/fragments/ProductCardFragment.java
+++ app/src/main/java/sg/com/bitwave/shoppingsystem/fragments/ProductCardFragment.java
@@ -1,10 +1,12 @@
 package sg.com.bitwave.shoppingsystem.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import sg.com.bitwave.shoppingsystem.R;
 
@@ -17,12 +19,21 @@ public class ProductCardFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
+}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_card, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_product_card, container, false);
+        Typeface robotoBlack = Typeface.createFromAsset(getActivity().getAssets(), "roboto-black.ttf");
+        Typeface robotoLight = Typeface.createFromAsset(getActivity().getAssets(), "roboto-light.ttf");
+        TextView tvTitle2 = (TextView) rootview.findViewById(R.id.tv_title_2);
+        TextView tvPrice2 = (TextView) rootview.findViewById(R.id.tv_price_2);
+        tvTitle2.setTypeface(robotoBlack);
+        tvPrice2.setTypeface(robotoLight);
+
+        return rootview;
     }
 }
